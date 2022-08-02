b53bffc2bcea664cc3d60ee9dd9710b69678f183
DaemosOriginal <83980110+DaemosOriginal@users.noreply.github.com>
2022-08-02 04:15:17 +0200

    automatische Galerie Generierung.

--- portfolio/portfolio.js
+++ portfolio/portfolio.js
@@ -1,6 +1,31 @@
-// listen for load event in the window
-window.addEventListener("load", function () {
+const dir = "/src/images"
+var numberOfImages = 9
+numberOfImages += 1;
+window.onload = function() {
+  var pos = document.getElementById("test")
+  for (var i = 1; i < numberOfImages; i++) {
+    var div = document.createElement('div')
+    div.classList.add('image');
+    div.style.backgroundImage = "url(src/images/"+i+".jpg)";
+    div.id = i;
 
-  
+    var input = document.createElement('input')
+    input.id = "input" + i;
+    input.type = "button";
+    //input.hidden = true;
 
-});
+    var label = document.createElement('label')
+    label.htmlFor = "input" + i;
+    label.id = "label" + i;
+
+    var script = document.createElement('script')
+    script.innerHTML = "console.log(\"Test\")";
+    document.body.appendChild(script)
+
+    pos.appendChild(input);
+    pos.appendChild(label);
+    label = document.getElementById("label" + i)
+    label.appendChild(div);
+  }
+
+}
