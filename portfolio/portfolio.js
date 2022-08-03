c649d577c9bae6fe9d6a949770ba17fed6fff478
DaemosOriginal <83980110+DaemosOriginal@users.noreply.github.com>
2022-08-03 22:21:35 +0200

    just some coding

--- portfolio/portfolio.js
+++ portfolio/portfolio.js
@@ -1,10 +1,12 @@
 const dir = "/src/images"
 var numberOfImages = 9
 numberOfImages += 1;
+
 window.onload = function() {
-  var pos = document.getElementById("test")
+  var pos = document.getElementById("layer1")
+
   for (var i = 1; i < numberOfImages; i++) {
-    var div = document.createElement('div')
+    var div = document.createElement('div');
     div.classList.add('image');
     div.style.backgroundImage = "url(src/images/"+i+".jpg)";
     div.id = i;
@@ -12,20 +14,31 @@ window.onload = function() {
     var input = document.createElement('input')
     input.id = "input" + i;
     input.type = "button";
-    //input.hidden = true;
+    input.name = "input";
+    input.hidden = true;
 
     var label = document.createElement('label')
     label.htmlFor = "input" + i;
     label.id = "label" + i;
 
-    var script = document.createElement('script')
-    script.innerHTML = "console.log(\"Test\")";
-    document.body.appendChild(script)
-
     pos.appendChild(input);
     pos.appendChild(label);
-    label = document.getElementById("label" + i)
+    label = document.getElementById("label" + i);
     label.appendChild(div);
+
+    document.getElementById("input" + i).addEventListener("click",myFunction);
   }
 
+  function myFunction(){
+    var id = this.id;
+    var list = id.split('');
+    var length  = list.length;
+    var len = length -= 5;
+    id = ""
+    for(var i = 0; i < len; i++){
+      var inew = i+5
+      id = id + list[inew];
+    }
+    console.log(id);
+  }
 }
