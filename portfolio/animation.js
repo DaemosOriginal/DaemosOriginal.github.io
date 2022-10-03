cee5c54d08b9ae378e3e3f3adc08b15f8594ecb8
DaemosOriginal <83980110+DaemosOriginal@users.noreply.github.com>
2022-10-03 15:11:56 +0200

    Some small update with an aniamtion

---
+++ portfolio/animation.js
@@ -0,0 +1,17 @@
+const background = document.getElementById('background');
+const scale = 250;
+
+window.addEventListener("mousemove", (e) => {
+    let pos;
+    if (window.innerWidth < window.innerHeight)
+    {
+         pos = [e.clientX/window.innerHeight*scale,e.clientY/window.innerHeight*scale];
+    }
+    else
+    {
+         pos = [e.clientX/window.innerWidth*scale,e.clientY/window.innerWidth*scale];
+    }
+    
+    console.log(pos)
+    background.style.backgroundPosition = `${pos[0]}px ${pos[1]}px`;
+});
