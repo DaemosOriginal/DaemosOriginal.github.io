d0156ddbc5f92d2f76015cbdc3d740fbeaa4f61c
DaemosOriginal <83980110+DaemosOriginal@users.noreply.github.com>
2022-10-05 19:19:55 +0200

    Update animation.js

--- portfolio/animation.js
+++ portfolio/animation.js
@@ -8,21 +8,21 @@ function sleep(miliseconds) {
 
 const background = document.getElementById('background');
 const scale = 250;
+if(window.innerHeight < window.innerWidth){
+     window.addEventListener("mousemove", (e) => {
+     let pos;
+     if (window.innerWidth < window.innerHeight)
+     {
+          pos = [e.clientX/window.innerHeight*scale,e.clientY/window.innerHeight*scale];
+     }
+     else
+     {
+          pos = [e.clientX/window.innerWidth*scale,e.clientY/window.innerWidth*scale];
+     }
 
-window.addEventListener("mousemove", (e) => {
-    let pos;
-    if (window.innerWidth < window.innerHeight)
-    {
-         pos = [e.clientX/window.innerHeight*scale,e.clientY/window.innerHeight*scale];
-    }
-    else
-    {
-         pos = [e.clientX/window.innerWidth*scale,e.clientY/window.innerWidth*scale];
-    }
-
-    background.style.backgroundPosition = `${-pos[0]}px ${-pos[1]}px`;
-});
-
+     background.style.backgroundPosition = `${-pos[0]}px ${-pos[1]}px`;
+     });
+}
 function tab (tabId, c,id)
 {
      document.getElementById(tabId)?.addEventListener('click',(e) =>{
